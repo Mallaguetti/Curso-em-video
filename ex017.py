0cb219468e1ae71a81151b9dca6b12ec720b1c35
Gabriel Mallaguetti <62891640+Mallaguetti@users.noreply.github.com>
2020-07-18 15:42:58 -0300

    Create ex017.py

---
+++ ex017.py
@@ -0,0 +1,4 @@
+from math import sqrt
+co = float(input("Digite o valor do cateto oposto: "))
+ca = float(input("Digite o valor do cateto adjacente: "))
+print("O valor da hipotenusa é {}".format(sqrt(co**2+ca**2)))
